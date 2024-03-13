63a9d25dd8a57cf7ad0a4030631b63589d3e1dbb
Gautam Raj <gautamraj996@gmail.com>
2024-03-13 11:37:13 +0530

    Add theme context

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,43 @@
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import { ThemeContextProvider } from "./contexts/ThemeContext.ts";
+import Products from "./components/Products/Products.tsx";
+import Product from "./components/Product/Product.tsx";
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route path="/" element={<Products />} />
+        <Route path="/product/:productId" element={<Product />} />
+      </>
+    )
+  );
+  const [themeMode, setThemeMode] = useState("light");
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+
+  useEffect(() => {
+    const htmlElement = document.querySelector("html");
+    if (htmlElement) {
+      htmlElement.classList.remove("light", "dark");
+      htmlElement.classList.add(themeMode);
+    }
+  }, [themeMode]);
   return (
     <>
-    
+      <ThemeContextProvider value={{ themeMode, lightTheme, darkTheme }}>
+        <RouterProvider router={router} />
+      </ThemeContextProvider>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
-import Products from './components/Products/Products.tsx'
-import Product from "./components/Product/Product.tsx"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <>
-    <Route path='/' element={<Products/>}/>
-    <Route path='/product/:productId' element={<Product/>}/>
-    </>
-  )
-)
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
-  </React.StrictMode>,
-)
+    <App />
+  </React.StrictMode>
+);
