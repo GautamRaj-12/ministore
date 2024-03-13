13642b3d4fa26c3cc5b3109bcd866ff04aaa41d1
Gautam Raj <gautamraj996@gmail.com>
2024-03-13 10:19:23 +0530

    Add search functionality

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, ChangeEvent } from 'react';
 
 interface Product {
   id: number;
@@ -15,14 +15,20 @@ interface Product {
 
 const Products: React.FC = () => {
   const [products, setProducts] = useState<Product[]>([]);
+  const [searchText, setSearchText] = useState<string>('');
+  const [displayedProducts, setDisplayedProducts] = useState<Product[]>([]);
+
+  const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchText(e.target.value);
+  };
 
   useEffect(() => {
     const fetchProducts = async () => {
       try {
         const response = await fetch('https://fakestoreapi.com/products');
         const data: Product[] = await response.json();
-        console.log(data);
         setProducts(data);
+        setDisplayedProducts(data); // Initially, display all products
       } catch (error) {
         console.error('Some error occurred', error);
       }
@@ -30,25 +36,47 @@ const Products: React.FC = () => {
     fetchProducts();
   }, []);
 
+  useEffect(() => {
+    const searchedData = () => {
+      const filteredProducts = products.filter((product) =>
+        product.title.toLowerCase().includes(searchText.toLowerCase())
+      );
+      console.log(filteredProducts);
+      setDisplayedProducts(filteredProducts);
+    };
+
+    searchedData();
+  }, [searchText, products]);
+
   return (
     <>
+      <section className="search w-[90%] mx-auto">
+        <div className='flex justify-center gap-2 my-4'>
+          <input
+            type="text"
+            placeholder='search your favourite items'
+            className='w-[60%] px-2 border-2 border-slate-500 outline-none'
+            value={searchText}
+            onChange={handleSearchChange}
+          />
+        </div>
+      </section>
       <section className='w-[90%] mx-auto'>
-        <div className='grid md:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-4'>
-          {products.map((product) => (
+        <div className='grid grid-cols-1 gap-4 md:grid-cols-4 sm:grid-cols-2'>
+          {displayedProducts.map((product) => (
             <div
               key={product.id}
-              className='border-2 p-2 grid grid-cols-1 gap-2'
+              className='grid grid-cols-1 gap-2 p-2 border-2'
             >
               <div>
                 <img src={product.image} alt='' className='w-[80%] h-40' />
               </div>
-
-              <div className='font-semibold text-2xl object-cover'>
+              <div className='object-cover text-2xl font-semibold'>
                 {product.title.length > 30
                   ? product.title.slice(0, 30) + '...'
                   : product.title}
               </div>
-              <div className='font-semibold text-xl'>{product.price}</div>
+              <div className='text-xl font-semibold'>{product.price}</div>
               <div>{product.description.slice(0, 100) + '...'}</div>
               <div>{product.category}</div>
               <div>{product.rating.rate}</div>
