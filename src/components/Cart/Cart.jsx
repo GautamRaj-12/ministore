a5bd02a1395306206eed8273d51e51ad0a531cdd
Gautam Raj <gautamraj996@gmail.com>
2024-03-22 10:59:34 +0530

    Add cart total feature

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,12 +3,28 @@ import { Link } from "react-router-dom";
 
 const Cart = () => {
   const cartItems = useSelector((store) => store.cart.items);
+  const totalPrice = cartItems.reduce((total, item) => total + item.price, 0);
   return (
     <>
+      <div className="flex items-center justify-center gap-4 mt-4">
+        <h2 className="text-3xl font-bold text-center">Shopping Cart</h2>
+        <div className="relative">
+          <i className="text-3xl fa-solid fa-cart-shopping"></i>
+          <span className="absolute flex items-center justify-center w-6 h-6 rounded-full -right-3 text-md top-4 bg-rose-500">
+            <p>{cartItems.length}</p>
+          </span>
+        </div>
+      </div>
+
       {cartItems.length <= 0 ? (
-        <div className="mt-4 text-6xl text-center">Cart is empty</div>
+        <div className="mt-32 text-center text-7xl text-rose-500">
+          <i className="fa-solid fa-heart-crack"></i>
+          <p className="text-lg font-semibold text-slate-500">
+            It is so empty here....
+          </p>
+        </div>
       ) : (
-        <div className="mt-4 w-[90%] mx-auto">
+        <div className="mt-8 w-[50%] mx-auto bg-slate-800 p-5">
           {cartItems.map((item) => (
             <div key={item.id} className="flex justify-between mb-2">
               <Link to={`/product/${item.id}`}>
@@ -17,6 +33,15 @@ const Cart = () => {
               <p>{item.price}</p>
             </div>
           ))}
+          <div className="flex justify-between border-t-2 total">
+            <p className="">Total</p>
+            <p className="">{totalPrice}</p>
+          </div>
+          <div className="flex justify-center mt-5">
+            <button className="flex justify-center p-2 rounded w-30 bg-rose-500/90">
+              Checkout
+            </button>
+          </div>
         </div>
       )}
     </>
