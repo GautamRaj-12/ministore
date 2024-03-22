b63e239795792cee807b4f452743c09e8153e417
Gautam Raj <gautamraj996@gmail.com>
2024-03-22 14:01:59 +0530

    Make modal responsive

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -14,7 +14,7 @@ const Modal = (props) => {
             : "pointer-events-auto"
         } bg-slate-700/70 justify-center items-center ${props.visibility}`}
       >
-        <div className="flex flex-col items-center justify-center w-1/4 gap-2 rounded-md bg-slate-700 h-1/4">
+        <div className="flex flex-col items-center justify-center md:w-1/4 w-[95%] gap-2 rounded-md bg-slate-700 md:h-1/4 h-1/2">
           <div>{props.text}</div>
           <button
             className="flex justify-center px-4 py-1 rounded w-30 bg-rose-500/90"
