e19c785bfda65f8d06bf708181e2ff3bd6d976e1
Gautam Raj <gautamraj996@gmail.com>
2024-03-25 18:51:01 +0530

    fix pointer events in cart notification

--- src/components/CartNotification/CartNotification.jsx
+++ src/components/CartNotification/CartNotification.jsx
@@ -2,7 +2,9 @@ const CartNotification = ({ isVisible }) => {
   return (
     <div
       className={`fixed p-2 rounded-sm top-2 right-2 bg-rose-500 transition-opacity duration-500 ${
-        isVisible ? "opacity-100" : "opacity-0"
+        isVisible
+          ? "opacity-100 pointer-events-auto"
+          : "opacity-0 pointer-events-none"
       }`}
     >
       <p>Added to cart</p>
